474ad0cf3b89b6621588dc14b826ab5c8de64d8e
Wenjun Kang <wkang2@bsd.uchicago.edu>
2020-01-30 13:16:12 -0600

    Replace curry to partialmethod

--- audit_log/middleware.py
+++ audit_log/middleware.py
@@ -1,10 +1,11 @@
 from django.db.models import signals
-from django.utils.functional import curry
+from functools import partialmethod
 from django.utils.deprecation import MiddlewareMixin
 
 from audit_log import registration, settings
 from audit_log.models import fields
 from audit_log.models.managers import AuditLogManager
+
 
 def _disable_audit_log_managers(instance):
     for attr in dir(instance):
@@ -34,23 +35,23 @@ class UserLoggingMiddleware(MiddlewareMixin):
             else:
                 user = None
             session = request.session.session_key
-            update_pre_save_info = curry(self._update_pre_save_info, user, session)
-            update_post_save_info = curry(self._update_post_save_info, user, session)
-            signals.pre_save.connect(update_pre_save_info,  dispatch_uid = (self.__class__, request,), weak = False)
-            signals.post_save.connect(update_post_save_info,  dispatch_uid = (self.__class__, request,), weak = False)
+            update_pre_save_info = partialmethod(self._update_pre_save_info, user, session)
+            update_post_save_info = partialmethod(self._update_post_save_info, user, session)
+            signals.pre_save.connect(update_pre_save_info, dispatch_uid=(self.__class__, request,), weak=False)
+            signals.post_save.connect(update_post_save_info, dispatch_uid=(self.__class__, request,), weak=False)
 
     def process_response(self, request, response):
         if settings.DISABLE_AUDIT_LOG:
             return
-        signals.pre_save.disconnect(dispatch_uid =  (self.__class__, request,))
-        signals.post_save.disconnect(dispatch_uid =  (self.__class__, request,))
+        signals.pre_save.disconnect(dispatch_uid=(self.__class__, request,))
+        signals.post_save.disconnect(dispatch_uid=(self.__class__, request,))
         return response
 
     def process_exception(self, request, exception):
         if settings.DISABLE_AUDIT_LOG:
             return None
-        signals.pre_save.disconnect(dispatch_uid =  (self.__class__, request,))
-        signals.post_save.disconnect(dispatch_uid =  (self.__class__, request,))
+        signals.pre_save.disconnect(dispatch_uid=(self.__class__, request,))
+        signals.post_save.disconnect(dispatch_uid=(self.__class__, request,))
         return None
 
     def _update_pre_save_info(self, user, session, sender, instance, **kwargs):
@@ -64,8 +65,7 @@ class UserLoggingMiddleware(MiddlewareMixin):
             for field in registry.get_fields(sender):
                 setattr(instance, field.name, session)
 
-
-    def _update_post_save_info(self, user, session, sender, instance, created, **kwargs ):
+    def _update_post_save_info(self, user, session, sender, instance, created, **kwargs):
         if created:
             registry = registration.FieldRegistry(fields.CreatingUserField)
             if sender in registry:
@@ -74,7 +74,6 @@ class UserLoggingMiddleware(MiddlewareMixin):
                     _disable_audit_log_managers(instance)
                     instance.save()
                     _enable_audit_log_managers(instance)
-
 
             registry = registration.FieldRegistry(fields.CreatingSessionKeyField)
             if sender in registry:
@@ -85,15 +84,11 @@ class UserLoggingMiddleware(MiddlewareMixin):
                     _enable_audit_log_managers(instance)
 
 
-
-
-
 class JWTAuthMiddleware(MiddlewareMixin):
     """
     Convenience middleware for users of django-rest-framework-jwt.
     Fixes issue https://github.com/GetBlimp/django-rest-framework-jwt/issues/45
     """
-
 
     def get_user_jwt(self, request):
         from rest_framework.request import Request
@@ -116,7 +111,7 @@ class JWTAuthMiddleware(MiddlewareMixin):
     def process_request(self, request):
         from django.utils.functional import SimpleLazyObject
         assert hasattr(request, 'session'),\
-        """The Django authentication middleware requires session middleware to be installed.
+            """The Django authentication middleware requires session middleware to be installed.
          Edit your MIDDLEWARE_CLASSES setting to insert 'django.contrib.sessions.middleware.SessionMiddleware'."""
 
         request.user = SimpleLazyObject(lambda: self.get_user_jwt(request))
